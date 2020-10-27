c37e735d28076d4f66d72047d9d4e0c1f5f5fe71
Yahao Yan <yahao_yan@outlook.com>
2020-10-27 16:12:41 -0400

    minor error fix

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*#snk1jrdj%t^9ihg1j$2rlgp$pymtvfxvr&ipng+39$nalqgr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
