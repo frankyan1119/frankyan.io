557b5152b7b5b31a07f6db0d5ee62f4e46e46ea2
Yahao Yan <yahao_yan@outlook.com>
2020-10-27 16:26:28 -0400

    minor error fix

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -144,3 +144,19 @@ EMAIL_HOST_PASSWORD = ''
 
 
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
