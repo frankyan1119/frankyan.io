f7d90e3eb33184c136d06def1945bf0e14bcf786
Yahao Yan <yahao_yan@outlook.com>
2020-10-27 16:12:26 -0400

    minor error fix

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '*#snk1jrdj%t^9ihg1j$2rlgp$pymtvfxvr&ipng+39$nalqgr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'frankyan.io']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
